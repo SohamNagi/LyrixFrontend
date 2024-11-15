5fdddb8392be10e95a67a63bbbc88d78c616823a
SohamNagi <nagisoham@gmail.com>
2024-11-15 02:18:52 -0500

    drop analytics

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,6 @@
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const router = createBrowserRouter([
   {
@@ -22,8 +20,6 @@ const router = createBrowserRouter([
 export default function App() {
   return (
     <div className="flex flex-col h-screen w-screen overflow-hidden">
-      <Analytics />
-      <SpeedInsights />
       <Navbar />
       <div className="flex-grow flex items-center justify-center">
         <RouterProvider router={router} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { injectSpeedInsights } from "@vercel/speed-insights";
-import { injectAnalytics } from "@vercel/analytics/sveltekit";
 import "./index.css";
 
 import App from "./app.tsx";
-injectSpeedInsights();
-injectAnalytics();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
