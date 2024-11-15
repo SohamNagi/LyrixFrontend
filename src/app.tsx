db09e26410f080c23e46c29f1186d1b0ebd20055
SohamNagi <nagisoham@gmail.com>
2024-11-15 02:14:35 -0500

    fixed analytics

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,8 @@ const router = createBrowserRouter([
 export default function App() {
   return (
     <div className="flex flex-col h-screen w-screen overflow-hidden">
+      <Analytics />
+      <SpeedInsights />
       <Navbar />
       <div className="flex-grow flex items-center justify-center">
         <RouterProvider router={router} />
